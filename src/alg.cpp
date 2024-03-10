577107cdcc61446d5030b2e0035cc721ace2e6cc
platna <160319937+platna@users.noreply.github.com>
2024-03-10 14:47:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,26 +3,48 @@
 #include "alg.h"
 
 
+
 double pown(double value, uint16_t n) {
 
+double result = 1.0;
+  for (uint16_t i = 1; i <= n; i++)
+  result = result * value;
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
 
+uint64_t result = 1;
+  for (uint16_t i = 1; i <= n; i++)
+  result = result * i;
+  return result;
 }
 
 double calcItem(double x, uint16_t n) {
 
+return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
 
+double result = 0.0;
+for (uint16_t i = 0; i <= count; i++)
+  result = result + calcItem(x, i);
+  return result;
 }
 
 double sinn(double x, uint16_t count) {
 
+double result = 0.0;
+for (uint16_t i = 1; i <= count; i++)
+  result = resuilt + pown(-1, i - 1) * calcItem(x, 2 * i - 1);
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
 
+double result = 0.0;
+for (uint16_t i = 1; i <= count; i++)
+  result = result + pown(-1, i - 1) * calcItem(x, 2 * i - 2);
+  return result;
 }
